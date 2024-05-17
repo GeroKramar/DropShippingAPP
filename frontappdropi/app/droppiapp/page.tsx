1b5321bd6d0510b10083cc9fbdcdb42108098c8e
GeroKramar <gerokramar@gmail.com>
2024-05-17 01:24:48 -0300

    arreglo de errores

--- frontappdropi/app/droppiapp/page.tsx
+++ frontappdropi/app/droppiapp/page.tsx
@@ -1,105 +1,221 @@
-"use client"
+"use client";
 import axios from "axios";
 import { useEffect, useRef, useState } from "react";
 import ProductsInfoTable from "@/components/ProductsInfoTable";
 import Toolbar from "@/components/Toolbar";
-import { Button, Label } from "flowbite-react";
-
-const API_ENDPOINT = "https://api.dropi.co/api/products/index";
-const LOCAL_ENDPOINT = "http://localhost:8080/api/products";
+import { Button } from "flowbite-react";
 
 export default function Page() {
-  const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(1);
+  const [tokendata, setTokendata] = useState<string>();
   const dropiIdRef = useRef(null);
-  const [token, setToken] = useState<string>()
 
-  // Helper function to chunk an array into smaller arrays of specified length
-  const chunkArray = (array, chunkSize) => {
-    return Array.from({ length: Math.ceil(array.length / chunkSize) }, (v, i) =>
-      array.slice(i * chunkSize, i * chunkSize + chunkSize),
-    );
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userToken = localStorage.getItem("token");
+      if (userToken) {
+        setTokendata(userToken);
+      }
+    }
+  }, []);
+
+  const [firstLoad, setFirstLoad] = useState(true);
+  const [products, setProducts] = useState([
+    {
+      dropiId: "cargando",
+      name: "cargando",
+      stock: "cargando",
+      price: "cargando",
+      nuevoStock: "cargando",
+    },
+  ]);
+  const [productsById, setProductsById] = useState([]);
+
+  function dividirEnMatriz(arrayOriginal, longitudSubarrays) {
+    let matriz = [];
+    for (let i = 0; i < arrayOriginal.length; i += longitudSubarrays) {
+      matriz.push(arrayOriginal.slice(i, i + longitudSubarrays));
+    }
+    return matriz;
+  }
+
+  const saveDayStockProducts = () => {
+    localStorage.setItem("Products", JSON.stringify(products));
   };
 
-  // Function to load products from API and send them to a local server
   const loadProducts = async () => {
     try {
-      const response = await axios.post(
-        API_ENDPOINT,
+      const res = await axios.post(
+        "https://api.dropi.co/api/products/index",
         {
           privated_product: false,
+          bod: null,
           stockmayor: 200,
           no_count: true,
           pageSize: 1000,
+          startData: 0,
           userVerified: true,
         },
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcHAuZHJvcGkuY286ODAiLCJpYXQiOjE3MTU5MTE2MTAsImV4cCI6MTcxNTk5ODAxMCwibmJmIjoxNzE1OTExNjEwLCJqdGkiOiI1VUlzQWxKVFY5cWFhd2hlIiwic3ViIjoxMDUzMTgsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEiLCJhdWQiOiJEUk9QSSIsInRva2VuX3R5cGUiOiJEUk9QSSJ9.T0mcAvB0xz--TveOE4XR1q-FV01CFdBqAx5xsst95b4"}`,
+            Authorization: `Bearer ${tokendata}`,
           },
-        },
+        }
       );
+      const products = res.data.objects.map((product) => ({
+        dropiId: product.id,
+        name: product.name,
+        stock: product.stock,
+        price: product.sale_price,
+      }));
 
-      const products = response.data.objects.map(
-        ({ id, name, stock, sale_price }) => ({
-          dropiId: id,
-          name,
-          stock,
-          price: sale_price,
-        }),
-      );
-
-      await axios.post(LOCAL_ENDPOINT, products);
-      setProducts(products);
+      await axios.post("http://localhost:8080/api/products", products);
     } catch (error) {
-      console.error("Failed to load products:", error);
+      console.error(error);
     }
   };
 
-  // Function to search for products by ID
   const searchById = async (dropiId) => {
     try {
-      const { data } = await axios.post(
-        API_ENDPOINT,
+      const res = await axios.post(
+        "https://api.dropi.co/api/products/index",
         {
           privated_product: false,
+          bod: null,
           no_count: true,
           pageSize: 1000,
+          startData: 0,
+          userVerified: true,
           keywords: dropiId,
         },
         {
           headers: {
             "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${tokendata}`,
           },
-        },
+        }
       );
-      return data.objects[0];
+      return res.data.objects[0];
     } catch (error) {
-      console.error("Search failed:", error);
-      return undefined;
+      console.error(error);
     }
   };
 
-  const handleSearchById = async () => {
-    const product = await searchById(dropiIdRef.current.value);
-    setProducts(product ? [product] : []);
+  const reLoadProducts = async (productosArray, setProductsBoolean = true) => {
+    let productsAux = await Promise.all(
+      productosArray.map(async (product) => {
+        let prod = await searchById(product.dropiId);
+        return prod;
+      })
+    );
+
+    productsAux = productsAux.filter((product) => product != undefined);
+    productsAux = productsAux.map((prodAux) => {
+      let productVerified = productosArray.find(
+        (prod) => prod.dropiId === prodAux.id
+      );
+      if (prodAux?.stock < productVerified?.stock) {
+        return {
+          dropiId: prodAux.id,
+          stockViejo: productVerified?.stock,
+          stockNuevo: prodAux?.stock,
+        };
+      }
+    });
+
+    productsAux = productsAux.filter((product) => product != undefined);
+
+    productsAux = productosArray.map((product) => {
+      let productExists = productsAux.find(
+        (prod) => prod.dropiId === product.dropiId
+      );
+      if (productExists) {
+        product.nuevoStock = productExists.stockNuevo;
+        return product;
+      } else {
+        return product;
+      }
+    });
+
+    const productsConCambios = productsAux.filter(
+      (product) => product.nuevoStock && product.nuevoStock !== "0.00"
+    );
+
+    if (setProductsBoolean) {
+      setProducts(productsAux);
+    } else {
+      return productsConCambios;
+    }
   };
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get(LOCAL_ENDPOINT);
-      const productChunks = chunkArray(data, 20);
-      setProducts(productChunks[0]);
-    };
-    fetchProducts();
-  }, []);
+  async function iteracionConRetraso(
+    i,
+    max,
+    retraso,
+    matriz,
+    newArrayProducts
+  ) {
+    if (i < max) {
+      const productsAux = await reLoadProducts(matriz[i], false);
+
+      if (productsAux.length > 0) {
+        newArrayProducts = newArrayProducts.sort((a, b) => {
+          if (
+            parseInt(a.stock) - parseInt(a.nuevoStock) >
+            parseInt(b.stock) - parseInt(b.nuevoStock)
+          ) {
+            return -1;
+          }
+          if (
+            parseInt(a.stock) - parseInt(a.nuevoStock) <
+            parseInt(b.stock) - parseInt(b.nuevoStock)
+          ) {
+            return 1;
+          }
+          return 0;
+        });
+        setProducts(newArrayProducts);
+      }
+
+      setTimeout(
+        () =>
+          iteracionConRetraso(
+            i + 1,
+            max,
+            retraso,
+            matriz,
+            [...newArrayProducts, ...productsAux]
+          ),
+        retraso
+      );
+    }
+  }
+
+  const filterWinningProducts = async (matriz) => {
+    iteracionConRetraso(0, matriz.length, 7000, matriz, []);
+  };
+
+  const iniciar = () => {
+    axios.get("http://localhost:8080/api/products").then((res) => {
+      let matriz = dividirEnMatriz(res.data, 20);
+      filterWinningProducts(matriz);
+    });
+  };
+
+  const [page, setPage] = useState(1);
+
+  const handleSearchById = async () => {
+    const dropiId = dropiIdRef.current.value;
+    if (dropiId) {
+      const product = await searchById(dropiId);
+      setProductsById([product]);
+    }
+  };
 
   return (
     <>
       <div className="mt-4 flex justify-evenly">
-        <div className="flex items-center  gap-2 text-center">
+        <div className="flex items-center gap-2 text-center">
           <input
             type="text"
             placeholder="Buscar por ID de dropi"
@@ -116,12 +232,7 @@ export default function Page() {
         <Button onClick={loadProducts} color="dark">
           Cargar productos
         </Button>
-        <Button
-          onClick={() =>
-            localStorage.setItem("Products", JSON.stringify(products))
-          }
-          color="dark"
-        >
+        <Button onClick={saveDayStockProducts} color="dark">
           Guardar productos en storage
         </Button>
         <Button
@@ -134,3 +245,4 @@ export default function Page() {
     </>
   );
 }
+
